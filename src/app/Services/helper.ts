420378b388176040050d828b6e456e4dadeeacff
Josias Hidalgo <jhidalgou@ucenfotec.ac.cr>
2023-11-12 00:47:42 -0600

    Update helper.ts

--- src/app/Services/helper.ts
+++ src/app/Services/helper.ts
@@ -1,5 +1,5 @@
 export const baseUrl = () => {
-    if(window.location.href.includes('localhost')){
+    if(window.location.href.includes('skytech-proyecto-gym.s3-website-us-east-1.amazonaws.com')){
         return 'http://3.223.92.25:8080/api';
     }
     return '';
