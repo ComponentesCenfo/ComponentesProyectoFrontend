7701639c32398ce8175780ad8c96169633f6db5d
Josias Hidalgo <jhidalgou@ucenfotec.ac.cr>
2023-11-12 00:25:51 -0600

    Update helper.ts

--- src/app/Services/helper.ts
+++ src/app/Services/helper.ts
@@ -1,6 +1,6 @@
 export const baseUrl = () => {
     if(window.location.href.includes('localhost')){
-        return 'http://localhost:8080/api';
+        return 'http://3.223.92.25:8080/api';
     }
     return '';
 }
