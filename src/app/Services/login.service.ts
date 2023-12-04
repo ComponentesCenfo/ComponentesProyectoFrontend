a2b5f76f794c55c4047a3f97fc14259f1e8fead0
Josías <110140894+JhidalgoU@users.noreply.github.com>
2023-12-04 17:46:11 -0600

    Revert "Training plan view"

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,10 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
     providedIn: 'root',
   })
   export class LoginService {
 
+    constructor(private http: HttpClient,private router: Router ){}
 
     public setUserClient(user: any) {
         localStorage.setItem('userClient', JSON.stringify(user));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,6 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LandingPageClientComponent } from './landing-page-client/landing-page-client.component';
 import { TrainerRegisterComponent } from './trainer-register/trainer-register.component';
 import { LandingPageTrainerComponent } from './landing-page-trainer/landing-page-trainer.component';
-import { TrainingPlanComponent } from './training-plan/training-plan.component';
-
-import { ExercisesComponent } from './exercises/exercises.component';
-import { ClientTrainingPlanComponent } from './client-training-plan/client-training-plan.component';
 
 @NgModule({
   declarations: [
@@ -34,10 +30,7 @@ import { ClientTrainingPlanComponent } from './client-training-plan/client-train
     HomePageComponent,
     LandingPageClientComponent,
     LandingPageTrainerComponent,
-    TrainerRegisterComponent,
-    TrainingPlanComponent,
-    ExercisesComponent,
-    ClientTrainingPlanComponent
+    TrainerRegisterComponent
   ],
   imports: [
     BrowserModule,
